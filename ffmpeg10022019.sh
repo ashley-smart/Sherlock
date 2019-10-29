0b7fbd0c8844dd12f10e5f017170ae1f5eb9b37e
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2019-10-29 11:33:03 -0700

    without spaces

--- ffmpeg10022019.sh
+++ ffmpeg10022019.sh
@@ -7,18 +7,19 @@
 ml system
 ml ffmpeg
 
+mkdir 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_testmk'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_2_Basler acA2440-35um (22467982)_20191002_122159351.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_2/V02frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_2.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_2/V02frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_3_Basler acA2440-35um (22467982)_20191002_130634371.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_3/V03frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_3.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_3/V03frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_4_Basler acA2440-35um (22467982)_20191002_141839901.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_4/V04frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_4.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_4/V04frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_5_Basler acA2440-35um (22467982)_20191002_145556421.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_5/V05frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_5.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_5/V05frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_6_Basler acA2440-35um (22467982)_20191002_173426909.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_6/V06frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_6.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_6/V06frame_%%07d.jpg'
 
-mkdir 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_7'
+
 
 
 
