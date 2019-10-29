cea3b5a5f9e6ca8ab994feaa4ccfadb4685fa3e9
ashley-smart <40184749+ashley-smart@users.noreply.github.com>
2019-10-29 11:37:48 -0700

    more no underscore

--- ffmpeg10022019.sh
+++ ffmpeg10022019.sh
@@ -9,15 +9,15 @@ ml ffmpeg
 
 mkdir 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Videotestmk'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_2.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_2/V02frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video2.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_2/V02frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_3.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_3/V03frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video3.m4v' -filter:v "crop=2322:1812:81:189" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_3/V03frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_4.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_4/V04frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video4.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_4/V04frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_5.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_5/V05frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video5.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_5/V05frame_%%07d.jpg'
 
-ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video_6.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_6/V06frame_%%07d.jpg'
+ffmpeg -i 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/Video6.m4v' -filter:v "crop=2304:1992:45:54" 'oak/stanford/groups/trc/data/Ashley2/PER/10022019/analysis/Video_6/V06frame_%%07d.jpg'
 
 
 
